6bea2a58e6154c29d62cda99ee439430eeb1cbd2
Sam Whited <sam@samwhited.com>
2014-11-18 11:40:19 -0500

    Load system-level and user-level config files

--- config.go
+++ config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"../logger"
 	"github.com/BurntSushi/toml"
+	"github.com/mitchellh/go-homedir"
 )
 
 var C map[string]interface{}
@@ -19,9 +20,24 @@ func init() {
 		logger.Err("Error decoding default config blob")
 	}
 
-	// TODO: Load config files from /etc/yokel/config and ~/.config/yokel/config here
+	// Load system-level config
+	_, err = toml.DecodeFile("/etc/yokel/config", C)
+	if err != nil {
+		logger.Info("No config found at `/etc/yokel/config'.")
+	}
 
-	// Load local dir config
+	// Load user-level config
+	home, err := homedir.Dir()
+	if err != nil {
+		logger.Info("No home directory for current user; not loading user-level config")
+	} else {
+		_, err = toml.DecodeFile(home+"/.config/yokel/config", C)
+		if err != nil {
+			logger.Info("No config found at `" + home + "/.config/yokel/config'.")
+		}
+	}
+
+	// Load local config
 	_, err = toml.DecodeFile("config", C)
 	if err != nil {
 		logger.Info("No config found at `./config'.")
