8341206e78d39c85908bfa4d54cf9fd3765fd577
Sam Whited <sam@samwhited.com>
2014-11-21 13:56:52 -0500

    Make config work as a standalone package

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
-# Go Auto Config
+# Go Config
 
-Automatically loads TOML config files in Go. Files are loaded from
+Loads TOML config files and string blobs in Go. Config is loaded into `config.C map[string]interface{}`. If
+`config.DefaultConfig` is set to a TOML blob, that is loaded on package init. If `config.ProjectName` is set, the
+following files are loaded (after `config.DefaultConfig` if it's set) in order:
 
-You can use automatically load config in Go by importing:
+	1. `/etc/<ProjectName>/config`
+	2. `~/.config/<ProjectName>/config`
+	3. `./config`
 
-    import "github.com/SamWhited/autoconfig"
+Otherwise you can do this yourself later. Note that errors are not reported if config loading fails at any point during
+init. They are simply ignored and the init process continues as if nothing has happened.
+
+Note that `config.ProjectName` is considered a trusted variable (there's no checking for strings such as `../`. You
+should NOT set the project name from user input.
+
+To import and use this package in Go:
+
+    import "github.com/SamWhited/config"
--- config.go
+++ config.go
@@ -1,29 +1,50 @@
+// Package
 package config
 
 import (
-	"../logger"
 	"github.com/BurntSushi/toml"
+	"github.com/SamWhited/logger"
 	"github.com/mitchellh/go-homedir"
 )
 
-var C map[string]interface{}
+var (
+	C             map[string]interface{}
+	DefaultConfig string
+	ProjectName   string
+)
 
-var defaultConfig string = `
-lang = "en_US"
-`
-
+// Initializes the config map if it hasn't already been initialized before package load.
 func init() {
-	C = make(map[string]interface{})
-	// Decode built in default config
-	_, err := toml.Decode(defaultConfig, C)
-	if err != nil {
-		logger.Err("Error decoding default config blob")
+	if C == nil {
+		C = make(map[string]interface{})
 	}
+	if DefaultConfig != nil && DefaultConfig != "" {
+		LoadBlob(DefaultConfig)
+	}
+	if ProjectName != nil && ProjectName != "" {
+		LoadProjectConfig(ProjectName)
+	}
+}
 
+// Loads a TOML blob into the config map provided by the package.
+func LoadBlob(blob string) (err error) {
+	_, err = toml.Decode(blob, C)
+}
+
+// Loads a TOML file into the config map provided by the package.
+func LoadFile(path string) (err error) {
+	_, err = toml.DecodeFile(path, C)
+}
+
+// Loads an entire project's config (system level, user level, and local config) into the map provided by the package.
+// Logs any errors that occur using the `github.com/SamWhited/logger` package.
+// TODO: Don't just log errors now that this is a separate package; push them to a channel or something and let the user
+// handle them.
+func LoadProjectConfig(project string) {
 	// Load system-level config
-	_, err = toml.DecodeFile("/etc/yokel/config", C)
+	err := LoadFile("/etc/" + projectName + "/config")
 	if err != nil {
-		logger.Info("No config found at `/etc/yokel/config'.")
+		logger.Info("No config found at `/etc/" + projectName + "/config'.")
 	}
 
 	// Load user-level config
@@ -31,14 +52,14 @@ func init() {
 	if err != nil {
 		logger.Info("No home directory for current user; not loading user-level config")
 	} else {
-		_, err = toml.DecodeFile(home+"/.config/yokel/config", C)
+		err = LoadFile(home + "/.config/" + projectName + "/config")
 		if err != nil {
-			logger.Info("No config found at `" + home + "/.config/yokel/config'.")
+			logger.Info("No config found at `" + home + "/.config/" + projectName + "/config'.")
 		}
 	}
 
 	// Load local config
-	_, err = toml.DecodeFile("config", C)
+	err = LoadFile("config")
 	if err != nil {
 		logger.Info("No config found at `./config'.")
 	}
